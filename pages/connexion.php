b6c23d0d55140f2c8eb212d540db3b92e9b0345e
jean Eude <jean Eude@Robin>
2020-05-31 15:00:23 +0200

    Implémentation des formulaires de connexion et d'inscription

--- pages/connexion.php
+++ pages/connexion.php
@@ -10,4 +10,22 @@
         include('../Include/header.inc.php');
         include('../Include/footer.inc.php');
     ?>
+<section id="connex">
+            <h1> Connexion : </h1>
+            <form id="form_contact" name="form_connexion" action="connexion.php" method="POST">
+                <div class="login-box">
+                    <img class="logo" src="../images/images.png"   width="20%">
+                        </br>
+                        </br>
+                        </br>
+                    <i class="fa fa-user" aria-hidden="true"></i>
+                    <input type="text" name="pseudo" id="pseudo" size="35" required placeholder="Identifiant" value="<?php if (isset($pseudo)) echo htmlentities(trim($pseudo)); ?>">
+                        </br>
+                    <i class="fa fa-lock" aria-hidden="true"></i>
+                    <input type="password" name="mdp" placeholder="Mot de passe" id="mdp" size="35" required >
+                    <input type="submit" name="connexion" value="Connexion">
+                    Pas de compte ? <a class="lien" href="inscription.php">S'inscrire</a>
+                </div>
+                </form>
+                </section>
 </html>
---
+++ pages/inscription.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!-- 
+     Page web créé dans le cadre du cours de web Dev le 03/04/2020
+     Auteur : Alexis Vedrenne
+     Email : alexisvedrenne482@gmail.com
+-->
+<html>
+    <?php 
+        include('../Include/head.inc.php');
+        include('../Include/header.inc.php');
+        include('../Include/footer.inc.php');
+    ?>
+ <section id="inscrit">
+     <h1>Formulaire d'inscription :</h1></br>
+        <p>Merci de bien remplir ces champs afin de continuer.</p></br>
+        <form action="trait-inscription.php" method="post" name="Inscription">
+            <div id="inscritsbox">
+                <h2>Identifiant</h2></br>
+                <div id="inscritstext">
+                    <div><label for="pseudo">Pseudo : (3 à 32 caractères)</label>   <input type="text" name="pseudo" id="pseudo" size="30" /> </div></br></br>
+                    <div><label for="mdp">Mot de passe : (4 à 50 caractères)</label>  <input type="password" name="mdp" id="mdp" size="30" /></div></br></br>
+                    <div><label for="mdp_verif">Mot de passe (vérification) :</label>  <input type="password" name="mdp_verif" id="mdp_verif" size="30" /></div></br></br>
+                    <div><label for="mail">E-mail :</label>  <input type="text" name="mail" id="mail" size="30" /></div></br></br>
+                    <div><label for="mail_verif">E-mail (vérification) :</label> <input type="text" name="mail_verif" id="mail_verif" size="30" /></div></br></br>
+                    <div><label for="date_naissance">Date de naissance : (JJ/MM/AAAA)</label>  <input type="text" name="date_naissance" id="date_naissance" size="30" /></div>
+                </div>
+                <div class="inscription"> <input type="submit" value="Inscription" /> </div>
+                <a class="lien" href="connexion.php">Retour</a></br></br>
+            </div>
+            
+        </form>
+    </section>
+</html>

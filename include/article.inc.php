65d1c908f8d1267b401e60ba7b3a1c457a183c32
Alexis Vedrenne <alexisvedrenne482@gmail.com>
2020-06-05 09:14:24 +0200

    Ajout des étoiles

--- include/article.inc.php
+++ include/article.inc.php
@@ -19,15 +19,17 @@ while ($ligne = $rest->fetch(PDO::FETCH_OBJ)) {
     echo'<h2>Casque ',$ligne->libelle,'</h2>';
     echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,
     '" alt="', $ligne->modele, '">';
-    echo '<p>','<span>',$ligne->nom,' ',$ligne->modele,'</span>','</br></br>','Les étoiles','</br></br>','Prix : ',
+    echo '<p>','<span>',$ligne->nom,' ',$ligne->modele,'</span>','</br>','<span3 class="rating"><!--
+            --><a id="dire" href="#1" title="Donner 5 étoile">★</a><!--
+            --><a href="#2" title="Donner 4 étoiles">★</a><!--
+            --><a href="#3" title="Donner 3 étoiles">★</a><!--
+            --><a href="#4" title="Donner 2 étoiles">★</a><!--
+            --><a href="#5" title="Donner 1 étoile">★</a></span3>',
+            '</br></br>','Prix : ',
             '<span1>',$ligne->prix,'€','</span1>','</br></br>','Quantité : ','<span2>', $ligne->stock,'</span2>', 
             '</br></br>','<i class="fa fa-shopping-cart"></i>','<input class="add" type="submit" value="Ajouter au panier" />','</br></br>',' ',
             '<i class="fa fa-credit-card"></i>','<input class="buy" type="submit" value="Acheter cet article" />','</p>';
     echo '<div class="commentaires">', '</br></br></br>','Commentaires client :','</div>';
-    echo '</br>', '<textarea  ></textarea>';
+    echo '</br>', '<textarea></textarea>';
     echo '</article>';
-    
 }
-
-
-echo '</section>';
